605bbebf2bce2a97d5532be0ff5af040e5acdb2f
Héctor Santamaría <hector.stamaria92@gmail.com>
2026-08-31 14:44:11 -0600

    First commit

---
+++ src/main/java/com/firstmultibranch/jenkins/MultibranchjenkinsApplication.java
@@ -0,0 +1,21 @@
+package com.firstmultibranch.jenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class MultibranchjenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultibranchjenkinsApplication.class, args);
+	}
+
+	@GetMapping("/main")
+	public String testSaludoMain(){
+		return "Saludando desde main";
+	}
+
+}
---
+++ src/test/java/com/firstmultibranch/jenkins/MultibranchjenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.firstmultibranch.jenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultibranchjenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
