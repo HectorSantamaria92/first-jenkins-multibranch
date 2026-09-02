d16f8e10b3b17851c2a299c58f041d96c533494e
Héctor Santamaría <hector.stamaria92@gmail.com>
2026-09-02 00:38:42 -0600

    Cambios

--- src/main/java/com/firstmultibranch/jenkins/MultibranchjenkinsApplication.java
+++ src/main/java/com/firstmultibranch/jenkins/MultibranchjenkinsApplication.java
@@ -16,7 +16,7 @@ public class MultibranchjenkinsApplication {
 
 	@GetMapping("/main")
 	public String testSaludoMain(){
-		return "Saludando desde main";
+		return "Saludando desde main v9";
 	}
 
 }
